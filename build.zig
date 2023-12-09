49e04efe74c8e07d103273ac398b6dbf22905cba
Eva Pace <eba.pachi@gmail.com>
2023-12-09 20:27:15 -0300

    add `run` command to `zig build`

--- build.zig
+++ build.zig
@@ -15,4 +15,13 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("gstreamer-1.0");
 
     b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
--- main.zig
+++ main.zig
@@ -5,20 +5,7 @@ const gst = @cImport({
 const glib = @cImport({
     @cInclude("glib.h");
 });
-// const gst = c.gst;
-// const glib = c.glib;
 
 pub fn main() void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // const allocator = gpa.allocator();
-
-    // const args = try std.os.argsAlloc(allocator);
-    // defer std.os.argsFree(allocator, args);
-    //
-    // gst.gst_init(args, args.len());
-    // main.zig:17:27: error: expected type '[*c]c_int', found '[*][*:0]u8'
     gst.gst_init(@ptrCast(&std.os.argv.len), @ptrCast(&std.os.argv.ptr));
 }
-//
-// var pipeline: gst.GstElement = undefined;
-// _ = pipeline;
