3af91a5f89bc0bf70d1a13dce313929e98277dea
Eva Pace <eba.pachi@gmail.com>
2023-12-09 21:38:50 -0300

    first example working!

--- main.zig
+++ main.zig
@@ -8,4 +8,24 @@ const glib = @cImport({
 
 pub fn main() void {
     gst.gst_init(@ptrCast(&std.os.argv.len), @ptrCast(&std.os.argv.ptr));
+
+    const pipeline: *gst.GstElement = gst.gst_parse_launch(
+        "playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+        null,
+    );
+    const bus: *gst.GstBus = gst.gst_element_get_bus(pipeline);
+    _ = gst.gst_element_set_state(pipeline, gst.GST_STATE_PLAYING);
+    const msg: *gst.GstMessage = gst.gst_bus_timed_pop_filtered(
+        bus,
+        gst.GST_CLOCK_TIME_NONE,
+        gst.GST_MESSAGE_ERROR | gst.GST_MESSAGE_EOS,
+    );
+
+    if (gst.GST_MESSAGE_TYPE(msg) == gst.GST_MESSAGE_ERROR) {
+        std.debug.print("an error ocurred bla bla bla\n", .{});
+    }
+
+    gst.gst_object_unref(bus);
+    _ = gst.gst_element_set_state(pipeline, gst.GST_STATE_NULL);
+    gst.gst_object_unref(pipeline);
 }
